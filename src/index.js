e5a2d89a9a5cb8442d8640eabd923f85a8fe4e47
Ryan Hirsch <ryan.hirsch@gmail.com>
2016-09-25 15:14:15 -0500

    fix minor lint issues and add winston

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import logger from './lib/logger';
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.json({ works: true });
+  res.json({
+    works: true,
+    version: '1.0.0',
+  });
 });
 
-const server = app.listen(process.env.PORT || 3000, () => {
-  console.log(`server is running on port ${server.address().port}`);
+const server = app.listen(process.env.PORT || 3000, () => { // eslint-disable-line no-process-env
+  logger.log('info', `server is running on port ${server.address().port}`);
 });
