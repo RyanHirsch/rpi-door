843c0ad591ef2a6b8bd71b8a41da9c32c0c5cd1d
Ryan Hirsch <ryan.hirsch@gmail.com>
2016-09-25 15:22:07 -0500

    add helmet and compression

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import compression from 'compression';
 import logger from './lib/logger';
 
 const app = express();
-
+app.use(helmet());
+app.use(compression());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.json({
     works: true,
-    version: '1.0.0',
+    version: '1.0.1',
   });
 });
 
